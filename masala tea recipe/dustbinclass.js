8dde3df69ade6d35122de896c8d0d9f9fbfe71ab
DrrishyaJayadev <75019734+DrrishyaJayadev@users.noreply.github.com>
2021-02-07 12:35:46 +0530

    Add files via upload

---
+++ masala tea recipe/dustbinclass.js
@@ -0,0 +1,7 @@
+class Dustbin extends BaseClass{
+    constructor(x,y){
+      super(x,y,80,80);
+      this.image = loadImage("dustbingreen.png");
+
+}
+}
---
+++ masala tea recipe/sketch.js
@@ -0,0 +1,12 @@
+MASALA TEA RECIPE
+
+ingredients:-
+
+one cup water
+one-inch piece of ginger
+one-inch piece of ginger
+one cardamom pod
+one tiny cinnamon stick
+two cloves
+a teaspoon of honey
+three tablespoons milk
